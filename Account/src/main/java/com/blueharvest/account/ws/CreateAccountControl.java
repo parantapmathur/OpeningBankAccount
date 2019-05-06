78135a650e144578eac55857a05b159efa2775f9
parantapmathur <30618598+parantapmathur@users.noreply.github.com>
2019-05-07 01:08:39 +0400

    h2 db

--- Account/src/main/java/com/blueharvest/account/ws/CreateAccountControl.java
+++ Account/src/main/java/com/blueharvest/account/ws/CreateAccountControl.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @author paran
+ * @author Parantap Mathur
  *
  */
 @RestController
---
+++ repository/src/main/java/com/blueharvest/repository/controler/Controler.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.blueharvest.repository.controler;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Parantap Mathur
+ *
+ */
+@RestController
+public class Controler {
+
+}
